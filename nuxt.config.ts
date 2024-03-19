846b38f3ee8fb368feabdea2190d1994c909d813
RudolfRukavina <rukavina.rudi@gmail.com>
2024-03-20 00:43:08 +0100

    Taxi Opatija Nuxt V1.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,16 +4,6 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
      viewport: 'width=device-width, initial-scale=1',
-     script: [
-      {
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-YH3FWH14LE',
-        async: true
-      },
-      {
-        src: '/google-tag-manager-script.js',
-        async: true
-      }
-    ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: './public/favicon.ico'}],
     },
   },
