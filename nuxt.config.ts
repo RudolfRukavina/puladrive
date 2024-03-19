7de40be42270c719e79ec2c543ab6734f6e8c1cb
RudolfRukavina <rukavina.rudi@gmail.com>
2024-03-19 17:57:39 +0100

    Taxi Opatija Nuxt V1.06

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
      viewport: 'width=device-width, initial-scale=1',
-      link: [{ rel: 'icon', type: 'image/x-con', href: './public/favicon.ico'}],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: './public/favicon.ico'}],
     },
   },
     modules: [
