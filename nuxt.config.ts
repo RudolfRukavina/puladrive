7d318c6ffa318ec62c5f94f492c08832037c05d1
RudolfRukavina <rukavina.rudi@gmail.com>
2024-03-20 20:27:22 +0100

    Taxi Opatija Nuxt V1.25

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'manifest', href: '/site.webmanifest' }
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
       ],
     },
   },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-swiper'
-  ],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-swiper"],
   tailwindcss: {
-    cssPath: '~/assets/css/input.css'
-  }
-})
+    cssPath: "~/assets/css/input.css",
+  },
+});
