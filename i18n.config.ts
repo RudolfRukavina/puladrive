8cc2c27561d55626d3fc7df02ab9eaaa4e5de53a
RudolfRukavina <rukavina.rudi@gmail.com>
2024-04-16 15:32:42 +0200

    TaxiOpatija - i18n browser based locale

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,6 @@
 export default defineI18nConfig(() => ({
   legacy: false,
-  locale: "en",
+  locale: (navigator.language.startsWith('hr') ? 'hr' : 'en'),
   messages: {
     en: {
       welcome: "Welcome",
